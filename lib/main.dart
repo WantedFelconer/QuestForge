6f40eefeb058d808c949ec700c1374fb78b18bf7
WantedFelconer <emon62224@gmail.com>
2026-03-01 23:37:35 +0600

    Added Some Hardcoded Tasks, It ain't much but it's progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
+  final List<Widget> TaskList = [
+    Text("Do the Chores"),
+    Text("Complete the Assignment"),
+    Text("Take wife out to dinner"),
+  ];
+
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(title: "QuestForge" ,home: Scaffold(appBar: AppBar(title: Text("_QuestForge_"),),body: Text("hello world")));
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "QuestForge",
+      home: Scaffold(
+        appBar: AppBar(title: Text("_QuestForge_")),
+        body: Center(child: Column(children: TaskList,)),
+      ),
+    );
   }
 }
